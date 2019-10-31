ba16f9e1f36a61d817d4e854b9ab2afddac5a76b
Connor Claxton <czclaxton@gmail.com>
2019-10-31 14:14:01 -0500

    added colors to an object for easier import/use

--- src/Shared_Styles/Style_Variables/colors.js
+++ src/Shared_Styles/Style_Variables/colors.js
@@ -1,5 +1,12 @@
-// COLOR VARIABLES
-export const Background = "#1c1c1c";
-export const Primary = "#c3f73a";
-export const Text = "#e5edcf";
-export const Dark = "#0c0c0c";
+// COLORS
+
+export const colors = {
+    background = "#1c1c1c",
+    primary = "#c3f73a",
+    text = "#e5edcf",
+    dark = "#0c0c0c"
+}
+
+
+
+
