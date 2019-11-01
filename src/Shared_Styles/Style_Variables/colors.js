a92bccb031d34b59a2044b97b0a911d1e8d0c0a2
Faye Skeen <fskeen@gmail.com>
2019-10-31 20:30:32 -0700

    Added random color picker and font variables

--- src/Shared_Styles/Style_Variables/colors.js
+++ src/Shared_Styles/Style_Variables/colors.js
@@ -6,3 +6,18 @@ export const colors = {
   text: "#e5edcf",
   dark: "#0c0c0c"
 };
+
+// RANDOM COLOR PICKER
+export const randomColor = () => {
+  const colorArray = ["#c3f73a", "#4AFF95", "#4AE838", "#39F7E7", "#13CDFF"]
+  const randomNum = Math.floor(Math.random() * colorArray.length)
+  const pick = colorArray[randomNum]
+  return pick;
+}
+
+// FONTS
+
+export const fonts = {
+  title: `'Bungee Shade', cursive;`,
+  body: `'Open Sans', sans-serif;`
+}
