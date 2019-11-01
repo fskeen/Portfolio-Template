eb8e409b7cf619485fb2ac2860e3cb7886f708b8
Faye Skeen <fskeen@gmail.com>
2019-10-31 22:22:36 -0700

    Continued work on about page + styling

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,22 +1,23 @@
 import React from "react";
-import {SkillsItem} from './About.styled'
+import { PageTitle, SubTitle, SkillsItem, Paragraph, SmallSubTitle, MainContainer } from './About.styled'
 
 const About = () => {
-  const skills = ["JavaScript", "React", "HTML", "CSS", "LESS", "React Router", "Node JS", "Express", "Redux", "Material UI", "styled-components", "SQL", "REST", "Git", "JSON", "Context API"]
+  const skills = ["JavaScript", "React", "HTML", "CSS", "LESS", "React Router", "Node JS", "Express", "Redux", "Material UI", "styled-components", "SQL", "RESTful APIs", "Git", "JSON", "Context API"]
   return (
-    <main>
-      <h2>About</h2>
-      <p>
-        Cupcake ipsum dolor sit amet wafer gingerbread tart. Chupa chups caramels fruitcake. Bonbon cookie croissant gingerbread macaroon sugar plum soufflé. Marshmallow lollipop cupcake toffee.
-      </p>
-      <p>
-        Sweet sesame snaps liquorice wafer. Lollipop halvah chocolate cake cheesecake. Bonbon dragée chocolate sweet chocolate tootsie roll.
-      </p>
-      <p>
-        Croissant cookie danish powder caramels sweet cookie chocolate bar dragée. Cake icing cotton candy halvah cake gummies candy canes. Chocolate bonbon cookie cookie dragée marzipan halvah tootsie roll. Gingerbread carrot cake sugar plum chocolate bar danish lollipop.
-      </p>
+    <MainContainer>
+      <PageTitle>About me</PageTitle>
+      <Paragraph>
+      Sweet sesame snaps liquorice wafer. Lollipop halvah chocolate cake cheesecake. Bonbon dragée chocolate sweet chocolate tootsie roll.
+      </Paragraph>
+      <Paragraph>
+      Croissant cookie danish powder caramels sweet cookie chocolate bar dragée. Cake icing cotton candy halvah cake gummies candy canes. Chocolate bonbon cookie cookie dragée marzipan halvah tootsie roll. Gingerbread carrot cake sugar plum chocolate bar danish lollipop.
+      </Paragraph>
       <section>
-        <h3>Skills</h3>
+        <SubTitle>Skills</SubTitle>
+        <Paragraph>
+          Cupcake ipsum dolor sit amet wafer gingerbread tart. Chupa chups caramels fruitcake. Bonbon cookie croissant gingerbread macaroon sugar plum soufflé. Marshmallow lollipop cupcake toffee.
+        </Paragraph>
+        <SmallSubTitle>I've worked with...</SmallSubTitle>
         <ul>
           {skills.map(skill => {
             return (
@@ -25,7 +26,7 @@ const About = () => {
           })}
         </ul>
       </section>
-    </main>
+    </MainContainer>
   );
 };
 
--- src/Components/About/About.styled.js
+++ src/Components/About/About.styled.js
@@ -1,19 +1,61 @@
 import styled from 'styled-components'
 import { colors, randomColor, fonts } from '../../Shared_Styles/Style_Variables/colors'
 
+export const MainContainer = styled.main`
+  max-width: 80rem;
+  width: 80%;
+`;
+
+export const PageTitle = styled.h2`
+  color: ${colors.primary};
+  font-family: ${fonts.title};
+  font-size: 6rem;
+  font-weight: 800;
+  padding: 1rem;
+  margin: 3rem 0 .1rem 0;
+`;
+
+export const SubTitle = styled.h3`
+  color: ${colors.primary};
+  font-family: ${fonts.title};
+  font-size: 5rem;
+  font-weight: 800;
+  padding: 1rem;
+  margin: 3rem 0 .1rem 0;
+  
+`;
+
+export const SmallSubTitle = styled.h4`
+  color: ${colors.primary};
+  font-family: ${fonts.title};
+  font-size: 2.25rem;
+  font-weight: 400;
+  padding: 1rem;
+  margin: 3rem 0 .1rem 0;
+  
+`;
+
+export const Paragraph = styled.p`
+  color: ${colors.text};
+  font-family: ${fonts.body};
+  font-size: 1.5rem;
+  line-height: 2rem;
+  padding: 1rem;
+`;
 
 // SKILLS SECTION
+
 
 export const SkillsItem = styled.li`
   margin: .5rem;
   display: inline-block;
-  padding:1.5rem;
-  font-size: 1.4rem;
+  padding: 1.2rem;
+  font-size: 1.6rem;
   font-family: ${fonts.body};
-  border: 2px solid ${randomColor};
+  border: 1px solid rgb(113, 119, 98);
   border-radius: .7rem;
-  transition: all 2s ease;
-  color: ${randomColor}}
+  transition: all 1s ease;
+  color: ${randomColor};
 
   &:hover {
     cursor: pointer;
