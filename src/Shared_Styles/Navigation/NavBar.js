030223bd0c9c7ccf437dabf9153dd039c52cea0d
Connor Claxton <czclaxton@gmail.com>
2019-11-03 17:04:15 -0600

    pulling in new changes

--- src/Shared_Styles/Navigation/NavBar.js
+++ src/Shared_Styles/Navigation/NavBar.js
@@ -1,26 +1,88 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import colors from "../Style_Variables/colors";
+import { colors } from "../Style_Variables/colors";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
 
 const NavBar = () => {
-  const links = ["Home", "About", "Skills", "Work", "Contact"];
-
   return (
-    <nav class="navBar">
-      <ul>
-        {links.map(link => {
-          return (
-            <li className="link" key={link}>
-              <Link to={`/${link !== "Home" ? link.toLowerCase() : ""}`}>
-                {link}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
+    <Nav>
+      <LinksList>
+        <ListItem>
+          <Link to="/">
+            <IconSpan>
+              <FontAwesomeIcon icon={faHome} size="s" />
+            </IconSpan>
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link to="/">
+            <LinkText className="LinkText">Home</LinkText>
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link to="/about">
+            <LinkText className="LinkText">About</LinkText>
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link to="/skills">
+            <LinkText className="LinkText">Skills</LinkText>
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link to="/work">
+            <LinkText className="LinkText">Work</LinkText>
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Link to="/contact">
+            <LinkText className="Text">Contact</LinkText>
+          </Link>
+        </ListItem>
+      </LinksList>
       <div>icons</div>
-    </nav>
+    </Nav>
   );
 };
 
+const Nav = styled.nav`
+  background-color: ${colors.dark};
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  height: 100vh;
+  max-width: 100px;
+  position: fixed;
+  padding: 0.75rem;
+`;
+
+const LinksList = styled.ul`
+  height: 250px;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: space-around;
+`;
+
+const ListItem = styled.li`
+  padding: 5px 0;
+  margin: 10px 0;
+`;
+
+const IconSpan = styled.span`
+  color: ${colors.primary};
+  font-size: 3em;
+  &:hover {
+    color: ${colors.text};
+  }
+`;
+
+const LinkText = styled.p`
+  color: ${colors.primary};
+
+  &:hover {
+    color: ${colors.text};
+  }
+`;
 export default NavBar;
