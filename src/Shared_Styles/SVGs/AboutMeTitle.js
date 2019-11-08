bd43d5fb230c45814c72fce6b71f0cd4bcb1cf4b
Faye Skeen <fskeen@gmail.com>
2019-11-07 23:54:02 -0800

    Created animated title for contact page + added polish

--- src/Shared_Styles/SVGs/AboutMeTitle.js
+++ src/Shared_Styles/SVGs/AboutMeTitle.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const SvgAboutMeTitle = props => (
-  <svg width={500} height={130} viewBox="0 0 185.208 34.396" {...props}>
+  <svg width={700} height={130} viewBox="0 0 185.208 34.396" {...props}>
     <g
       aria-label="About me"
       style={{
