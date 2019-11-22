dca6e07f6f5736386c8e3fdce066f2e6b534c7b8
Connor Claxton <czclaxton@gmail.com>
2019-11-21 18:29:22 -0600

    wip

--- src/Components/MyWork/Display/Display.styled.js
+++ src/Components/MyWork/Display/Display.styled.js
@@ -12,12 +12,46 @@ height: 300px
 border: 1px solid pink
 `;
 
-const fadein = keyframes`
-  0% {
-    opacity: 1
+export const Button = styled.button`
+  margin: 0.5rem;
+  width: 22rem;
+  display: inline-block;
+  padding: 1rem;
+  font-size: 2.2rem;
+  font-family: ${fonts.body};
+  border: 2px solid rgb(113, 119, 98);
+  background: none;
+  border-radius: 0.7rem;
+  transition: all 1s ease;
+  color: ${randomColor};
+  align-self: center;
 
+  &:hover {
+    cursor: pointer;
+    color: black;
+    border-color: black;
+    background-color: #f9ffea;
+    transition: all 0.2s ease;
+    text-shadow: 2px 2px 0 ${colors.primary};
+    box-shadow: 2px 2px 0px 0px ${colors.primary};
   }
-  100% {
-    opacity: 0
+
+  @media ${device.mobileS} {
+    max-width: 60rem;
   }
 `;
+
+export const HoverCard = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+// const fadein = keyframes`
+//   0% {
+//     opacity: 1
+
+//   }
+//   100% {
+//     opacity: 0
+//   }
+// `;
--- src/Components/MyWork/Display/DisplayCard.js
+++ src/Components/MyWork/Display/DisplayCard.js
@@ -1,19 +1,33 @@
 import React from "react";
 import styled from "styled-components";
-import { CardContainer } from "./Display.styled";
+import { CardContainer, Button, HoverCard } from "./Display.styled";
 
 const DisplayCard = props => {
   const { title, image, skills, deployedUrl, githubUrl } = props.project;
-  console.log(image);
-  const Card = styled.div`
-  width: 100%
-  height: 100%
-  background-image: url(${image})
 
+  const Card = styled.div`
+    width: 100%;
+    height: 100%;
+    background-image: url(${image});
+    opacity: 1;
+    button {
+      display: none;
+    }
+    &:hover {
+      transition: opacity 0.5s ease-in-out;
+      opacity: 0;
+      button {
+        opacity: 1
+        display: block;
+      }
+    }
   `;
   return (
     <CardContainer>
-      <Card />
+      <Card style={{ border: "2px solid orange" }} />
+      <HoverCard style={{ border: "2px solid pink" }}>
+        <Button>Learn More</Button>
+      </HoverCard>
     </CardContainer>
   );
 };
