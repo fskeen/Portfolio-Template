9966bc479551b7cad5d9f4a52ddee071833b1cdc
Connor Claxton <czclaxton@gmail.com>
2019-11-27 19:09:16 -0600

    update

--- src/Components/MyWork/MyWork.js
+++ src/Components/MyWork/MyWork.js
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import DisplayCard from "./Display/DisplayCard";
 
-import { Modal } from 'styled-react-modal';
+import { Modal } from "styled-react-modal";
 import { MainContainer, DisplayContainer } from "./MyWork.styled";
 import {
   SubTitle,
@@ -12,27 +12,31 @@ import { projects } from "./projects";
 
 const MyWork = () => {
   const [projectList, setProjectList] = useState(projects);
-  const [selectedSkill, setSelectedSkill] = useState("All")
+  const [selectedSkill, setSelectedSkill] = useState("All");
 
   const skillList = ["All", "JavaScript", "React.js", "Node.js"];
 
   useEffect(() => {
-    const filtered = projects.filter(project => project.primarySkills.includes(selectedSkill))
+    const filtered = projects.filter(project =>
+      project.primarySkills.includes(selectedSkill)
+    );
     if (selectedSkill === "All") {
-      setProjectList(projects)
-      console.log(projectList)
+      setProjectList(projects);
+      console.log(projectList);
     } else {
-      setProjectList(filtered)
-      console.log(filtered)
+      setProjectList(filtered);
+      console.log(filtered);
     }
-  }, [selectedSkill]) 
+  }, [selectedSkill]);
 
   return (
     <MainContainer>
-    <div>
-    {skillList.map((skill, i) => (
-      <button onClick={() => setSelectedSkill(skill)} key={i}>{skill}</button>
-      ))}
+      <div>
+        {skillList.map((skill, i) => (
+          <button onClick={() => setSelectedSkill(skill)} key={i}>
+            {skill}
+          </button>
+        ))}
       </div>
       <DisplayContainer>
         {projectList.map((project, i) => {
