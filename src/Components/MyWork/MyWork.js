6bd7d36946f8e286eb90920e5dc2eec7376d23ea
Faye Skeen <fskeen@gmail.com>
2019-11-29 16:03:58 -0800

    tweaking works page + adding condt'l active class

--- src/Components/MyWork/MyWork.js
+++ src/Components/MyWork/MyWork.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
 import DisplayCard from "./Display/DisplayCard";
-
+import MyWorkTitle from '../../Shared_Styles/SVGs/MyWorkTitle'
+import {StyledSVGTitle} from '../About/About.styled'
 import { Modal } from "styled-react-modal";
 import {
   MainContainer,
   DisplayContainer,
   FilterButton,
   FilterButtonWrapper,
-  FilterButtonMenu
+  FilterButtonMenu,
+  Para
 } from "./MyWork.styled";
 import {
   SubTitle,
@@ -34,11 +36,17 @@ const MyWork = () => {
 
   return (
     <MainContainer>
+      <StyledSVGTitle>
+        <MyWorkTitle />
+      </StyledSVGTitle>
+      <Para >
+        Cupcake ipsum dolor sit amet. Apple pie chocolate cake croissant cupcake gummi bears cupcake. Pudding cheesecake toffee tootsie roll cupcake. Macaroon lemon drops pie icing macaroon. Danish jujubes jujubes pudding chocolate. Jelly beans chocolate bar muffin soufflé candy canes cookie. Carrot cake candy gingerbread dessert wafer oat cake sesame snaps pastry.
+      </Para>
+      <DisplayContainer>
       <FilterButtonMenu>
-        <FilterButtonWrapper>
           {skillList.map((skill, i) => (
             <FilterButton
-              className="filter"
+              className={`filter ${skill === selectedSkill && "active"}`}
               data-filter={skill}
               onClick={() => setSelectedSkill(skill)}
               key={i}
@@ -46,9 +54,7 @@ const MyWork = () => {
               {skill}
             </FilterButton>
           ))}
-        </FilterButtonWrapper>
       </FilterButtonMenu>
-      <DisplayContainer>
         {projectList.map((project, i) => {
           return <DisplayCard key={i} project={project} />;
         })}
